9f1a3fd9a837703cdff7f3d1b20a459e115d6df1
Ruihao_Zhang <zhangrh16@mails.thu.edu.cn>
2024-11-12 18:54:31 -0500

    Implement AutoGen-based multi-agent system

--- nq_agents/__init__.py
+++ nq_agents/__init__.py
@@ -1,5 +1,6 @@
 from .multi_agent import NQMultiAgent
+from .multi_agent_autogen import MultiAgentAutogen
 from .evaluation import evaluate_predictions
 from .data_extraction import sample_examples, write_jsonl
 
-__all__ = ['NQMultiAgent', 'evaluate_predictions', 'sample_examples', 'write_jsonl'] 
+__all__ = ['NQMultiAgent', 'evaluate_predictions', 'sample_examples', 'write_jsonl', 'MultiAgentAutogen'] 
